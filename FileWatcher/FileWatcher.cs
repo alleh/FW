1b0a40d8fae1c60513b5b424ccb27703129410c3
Alexander Augustsson <alex@alleh.io>
2015-03-10 14:18:24 +0100

    small fix

--- FileWatcher/FileWatcher.cs
+++ FileWatcher/FileWatcher.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -9,14 +10,25 @@ namespace FileWatcher
 {
     class FileWatcher
     {
-        private string _folderToWatch = "C:/Users/alc/Desktop/folder1";
-        private string _copyDestination = "C:/Users/alc/Desktop/folder2";
+        private readonly string _folderToWatch;
+        //private string _copyDestination = "C:/Users/alc/Desktop/folder2";
+        private readonly List<string> _copyDestinations; //= new List<string>();
+        //private List<DirectoryInfo> targetFolder = new List<DirectoryInfo>();
+
+        public FileWatcher(string source, List<string> destination)
+        {
+            _folderToWatch = source;
+            _copyDestinations = destination;
+        }
 
         public void Start()
         {
+            //_copyDestinations.Add(@"C:\Repos\folder2");
+            //_copyDestinations.Add(@"C:\Repos\folder3");
+            //_copyDestinations.Add(@"C:\Repos\folder4");
+
             var watcher = new FileSystemWatcher(_folderToWatch);
-            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
-                | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
 
             watcher.Changed += new FileSystemEventHandler(OnChanged);
             watcher.Created += new FileSystemEventHandler(OnChanged);
@@ -24,6 +36,7 @@ namespace FileWatcher
             watcher.Renamed += new RenamedEventHandler(OnRenamed);
             watcher.Error += new ErrorEventHandler(OnError);
             watcher.EnableRaisingEvents = true;
+            watcher.IncludeSubdirectories = true;
 
             Console.WriteLine("Press \'Enter\' to quit.");
             Console.ReadLine();
@@ -31,19 +44,30 @@ namespace FileWatcher
 
         private void OnChanged(object source, FileSystemEventArgs e)
         {
-            var sourceFolder = new DirectoryInfo(_folderToWatch);
-            var targetFolder = new DirectoryInfo(_copyDestination);
 
-            CopyAll(sourceFolder, targetFolder);
+            var now = DateTime.Now.Subtract(TimeSpan.FromMinutes(1));
+
+            var sourceFolder = new DirectoryInfo(_folderToWatch);
+
+            foreach (var folder in _copyDestinations)
+            {
+                var targetFolder = new DirectoryInfo(folder);
+                CopyAll(sourceFolder, targetFolder, now);
+            }
+
             Console.WriteLine("File {0} {1}", e.FullPath, e.ChangeType.ToString());
-            
         }
 
         private void OnRenamed(object source, RenamedEventArgs e)
         {
+            var now = DateTime.Now.Subtract(TimeSpan.FromMinutes(1));
+
             var sourceFolder = new DirectoryInfo(_folderToWatch);
-            var targetFolder = new DirectoryInfo(_copyDestination);
-            CopyAll(sourceFolder, targetFolder);
+            foreach (var folder in _copyDestinations)
+            {
+                var targetFolder = new DirectoryInfo(folder);
+                CopyAll(sourceFolder, targetFolder, now);
+            }
             Console.WriteLine("File {0} {2} to {1}", e.OldFullPath, e.FullPath, e.ChangeType.ToString());
         }
 
@@ -56,7 +80,7 @@ namespace FileWatcher
             }
         }
 
-        public void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        public void CopyAll(DirectoryInfo source, DirectoryInfo target, DateTime since)
         {
 
             if (Directory.Exists(target.FullName) == false)
@@ -64,27 +88,55 @@ namespace FileWatcher
                 Directory.CreateDirectory(target.FullName);
             }
 
-            foreach (var file in target.GetFiles())
-            {
-                File.Delete(file.FullName);
-            }
+            //foreach (var file in target.GetFiles("*", SearchOption.AllDirectories))
+            //{
+            //    File.Delete(file.FullName);
+            //}
 
-            foreach (var file in source.GetFiles())
+            foreach (var file in source.GetFiles("*", SearchOption.AllDirectories).Where(f => f.LastWriteTime > since))
             {
+
+                var path = file.FullName.Replace(source.FullName, target.FullName);
+
                 Console.WriteLine(@"Copying {0}\{1}", target.FullName, file.Name);
-                file.CopyTo(Path.Combine(target.ToString(), file.Name), true);
+                try
+                {
+                    file.CopyTo(path, true);
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine("ERROR: " + exc.Message);
+                }
             }
 
-            foreach (var SourceSubDir in source.GetDirectories())
-            {
-                var targetSubDir = target.CreateSubdirectory(SourceSubDir.Name);
-                CopyAll(SourceSubDir, targetSubDir);
-            }
+
+
+            //foreach (var SourceSubDir in source.GetDirectories())
+            //{
+            //    var targetSubDir = target.CreateSubdirectory(SourceSubDir.Name);
+            //    CopyAll(SourceSubDir, targetSubDir);
+            //}
         }
 
         static void Main(string[] args)
         {
-            var watch = new FileWatcher();
+
+            var source = ConfigurationManager.AppSettings["Source"];
+            var destinations = new List<string>();
+            var index = 1;
+            while (true)
+            {
+                var dest = ConfigurationManager.AppSettings["Destination" + index];
+                if (string.IsNullOrEmpty(dest))
+                {
+                    break;
+                }
+
+                destinations.Add(dest);
+                index++;
+            }
+
+            var watch = new FileWatcher(source, destinations);
 
             watch.Start();
         }
