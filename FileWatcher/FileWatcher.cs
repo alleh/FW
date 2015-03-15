ad131cf8f0d1af3596496e559bf93f2151aa8f13
Alex <alex@alleh.io>
2015-03-15 19:34:05 +0100

    fix

--- FileWatcher/FileWatcher.cs
+++ FileWatcher/FileWatcher.cs
@@ -117,28 +117,5 @@ namespace FileWatcher
             //    CopyAll(SourceSubDir, targetSubDir);
             //}
         }
-
-        static void Main(string[] args)
-        {
-
-            var source = ConfigurationManager.AppSettings["Source"];
-            var destinations = new List<string>();
-            var index = 1;
-            while (true)
-            {
-                var dest = ConfigurationManager.AppSettings["Destination" + index];
-                if (string.IsNullOrEmpty(dest))
-                {
-                    break;
-                }
-
-                destinations.Add(dest);
-                index++;
-            }
-
-            var watch = new FileWatcher(source, destinations);
-
-            watch.Start();
-        }
     }
 }
---
+++ FileWatcher/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var source = ConfigurationManager.AppSettings["Source"];
+            var destinations = new List<string>();
+            var index = 1;
+            while (true)
+            {
+                var dest = ConfigurationManager.AppSettings["Destination" + index];
+                if (string.IsNullOrEmpty(dest))
+                {
+                    break;
+                }
+
+                destinations.Add(dest);
+                index++;
+            }
+
+            var watch = new FileWatcher(source, destinations);
+
+            watch.Start();
+        }
+    }
+}
